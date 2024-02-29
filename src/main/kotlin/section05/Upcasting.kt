4b46d155a73b9be5ac4bb91b87a9ce47a2e1c6b0
power <x.oybek94@gmail.com>
2024-02-29 11:20:02 +0900

    Initial comment

---
+++ src/main/kotlin/section05/Upcasting.kt
@@ -0,0 +1,38 @@
+package section05
+
+import sun.util.locale.provider.LocaleResources.trace
+
+interface Shape{
+    fun draw():String
+    fun erase():String
+}
+class Circle:Shape{
+    override fun draw() = "Circle.draw"
+
+    override fun erase() = "Circle.erase"
+
+}
+class Square:Shape{
+    override fun draw() = "Square.draw"
+
+    override fun erase() = "Square.erase"
+    fun color() = "Square.color"
+
+}
+class Triangle:Shape{
+    override fun draw() = "Triangle.draw"
+
+    override fun erase() = "Triangle.erase"
+    fun rotate() = "Triangle.rotate"
+}
+//The show() function accepts any Shape:
+// Upcasting/Drawing.kt
+//package upcasting
+//import atomictest.*
+fun show(shape: Shape){
+    trace("Show: ${shape.draw()}")
+}
+fun main() {
+    listOf(Circle(), Square(), Triangle())
+        .forEach(::show)
+}
