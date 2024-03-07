2d87982c7c4c107e18fcbf995ae14a0d0dd5cacc
power <x.oybek94@gmail.com>
2024-03-07 14:06:38 +0900

    Initial comment

---
+++ src/main/kotlin/section05/CompanionObject06.kt
@@ -0,0 +1,20 @@
+package section05
+
+import kotlin.Exception
+
+
+class Exception(
+    val value:Int
+):Exception("wron value: $value")
+
+open class Exception2(
+    description:String
+):Exception(description)
+open class Exception3(
+    description:String
+):Exception2(description)
+
+fun main(){
+
+}
+
